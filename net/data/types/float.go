73d313d6a2b13f69ba71a982a4ce8d4fbb22ffcd
CyberBunker <chinahdj1@gmail.com>
2020-07-26 22:15:34 +0800

    fix: add float double uuid type func

--- net/data/types/float.go
+++ net/data/types/float.go
@@ -1,4 +1,39 @@
 package types
 
+import "math"
+
 type Float float32
+
+func (f Float) Encode(w Writer) (err error) {
+	err = Long(math.Float32bits(float32(f))).Encode(w)
+	return
+}
+
+func (d *Float) Decode(r Reader) (n int, err error) {
+	var long Long
+
+	if n, err = long.Decode(r); err != nil {
+		return
+	}
+
+	*d = Float(math.Float32frombits(uint32(long)))
+	return
+}
+
 type Double float64
+
+func (d Double) Encode(w Writer) (err error) {
+	err = Long(math.Float64bits(float64(d))).Encode(w)
+	return
+}
+
+func (d *Double) Decode(r Reader) (n int, err error) {
+	var long Long
+
+	if n, err = long.Decode(r); err != nil {
+		return
+	}
+
+	*d = Double(math.Float64frombits(uint64(long)))
+	return
+}
--- net/data/types/uuid.go
+++ net/data/types/uuid.go
@@ -1,3 +1,19 @@
 package types
 
-type UUID interface{}
+import (
+	"io"
+
+	"github.com/google/uuid"
+)
+
+type UUID uuid.UUID
+
+func (u UUID) Encode(w Writer) (err error) {
+	_, err = w.Write(u[:])
+	return
+}
+
+func (u *UUID) Decode(r Reader) (n int, err error) {
+	n, err = io.ReadFull(r, (*u)[:])
+	return
+}
